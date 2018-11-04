f0401b950a869d01a572d417664d94c04e73f873
huangyiting <18716002201@163.com>
2018-11-04 22:37:30 +0800

    render Submenu + Menu.Item

--- src/components/common/nav/nav.js
+++ src/components/common/nav/nav.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Menu, Icon } from 'antd'
+import menuConfig from './config.js'
+
+const { SubMenu } = Menu 
 
 class NavMenu extends React.Component {
     constructor() {
@@ -7,10 +10,32 @@ class NavMenu extends React.Component {
         this.state = {
         }
     }
+    renderSubmenu = menu => {
+        return menu.map(item => {
+            let res = null
+            if(item && item.subs) {
+                res = (
+                    <SubMenu key={item.title} title={item.title}>
+                        {this.renderMenuItem(item.subs)}
+                    </SubMenu>
+                )
+            } else {
+                res = (
+                    <Menu.Item key={item.title}>{item.title}</Menu.Item>
+                )
+            }
+            return res
+        })
+    }
+    renderMenuItem = (params = []) => {
+        return params.map(item => (
+            <Menu.Item key={item.title}>{item.title}</Menu.Item>
+        ))
+    }
     render() {
         return(
             <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} style={{ background: '#282c34' }}>
-                <Menu.Item key="1">
+                {/* <Menu.Item key="1">
                     <Icon type="user" />
                     <span>nav 1</span>
                 </Menu.Item>
@@ -21,7 +46,8 @@ class NavMenu extends React.Component {
                 <Menu.Item key="3">
                     <Icon type="upload" />
                     <span>nav 3</span>
-                </Menu.Item>
+                </Menu.Item> */}
+                {this.renderSubmenu(menuConfig.menu)}
             </Menu>
         )
     }
