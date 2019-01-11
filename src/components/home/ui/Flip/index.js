c72c9d73e4eb408c8ccd372dd822d52c6cd57d3e
YiTing Huang <huangyiting@sunlands.com>
2019-01-11 18:44:21 +0800

    flip

--- src/components/home/ui/Flip/index.js
+++ src/components/home/ui/Flip/index.js
@@ -1,23 +1,94 @@
-import React from 'react'
-import { Row, Col } from 'antd'
+/*
+ * @Description: 让原来的不动的话，就在蒙一层
+ * @Author: your name
+ * @Date: 2019-01-09 17:38:03
+ * @LastEditTime: 2019-01-11 18:38:24
+ * @LastEditors: Please set LastEditors
+ */
 
+import React from 'react'
+import style from './flip.scss'
+
+const OBJECT = {}
 
 class Flip extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            classname: true,
+            No: '',
+        }
     }
 
     componentDidMount() {
     }
 
+    changeAnimate = index => {
+        const first = OBJECT[index].getBoundingClientRect()
+        const { classname } = this.state
+        this.setState({
+            classname: !classname,
+            No: index,
+        },
+        () => {
+            OBJECT[index].classList.add('box-container-change')
+            const last = OBJECT[index].getBoundingClientRect()
+            const deltaX = first.left - last.left
+            const deltaY = first.top - last.top
+            const deltaW = first.width / last.width
+            const deltaH = first.height / last.height
+            OBJECT[index].animate(
+                [
+                    {
+                        transformOrigin: 'top left',
+                        transform: `translate(${deltaX}px, ${deltaY}px)
+                                    scale(${deltaW}, ${deltaH}) `,
+                    },
+                    {
+                        transformOrigin: 'top left',
+                        transform: 'none',
+                    },
+                ],
+                {
+                    duration: 1000,
+                    easing: 'ease-in-out',
+                    fill: 'both',
+                },
+            )
+        },
+        )
+    }
+
     render() {
+        const { classname, No } = this.state
+        const styleAfter = {
+            width: '400px',
+            height: '400px',
+            backgroundColor: 'chocolate',
+            position: 'absolute',
+            left: '500px',
+            top: '200px',
+            zIndex: '2',
+        }
+        const styles = classname ? {} : styleAfter
         return (
-            <Row gutter={4} style={{ marginTop: '20px' }}>
-                <Col span={12}>
-                    111
-                </Col>
-            </Row>
+            <div className={style['box-wrap']}>
+                {
+                    (() => (
+                        Object.keys(new Array(21).join(',')).map((item, index) => (
+                            <div
+                                key={Math.random()}
+                                style={No === index ? styles : {}}
+                                className={style['box-container']}
+                                onClick={this.changeAnimate.bind(this, index)}
+                                ref={node => { OBJECT[index] = node }}
+                            >
+                                {index}
+                            </div>
+                        ))
+                    ))()
+                }
+            </div>
         )
     }
 }
